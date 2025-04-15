13ad159e450052cda6fec76ff89f74ae246f2679
hemilioaraujo <hemilioaraujo@gmail.com>
2025-04-15 19:44:35 -0300

    feat: add Menu component

---
+++ src/components/Menu.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav style="padding: 1rem; background-color: #f0f0f0; margin-bottom: 1rem">
+    <RouterLink to="/" style="margin-right: 1rem">Home</RouterLink>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
