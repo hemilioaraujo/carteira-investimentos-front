333679711d4fd0f89b57cb21bec1c4c834174341
hemilioaraujo <hemilioaraujo@gmail.com>
2025-04-16 15:57:50 -0300

    style: adjust menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,13 +1,29 @@
 <template>
-  <nav style="padding: 1rem; background-color: #f0f0f0; margin-bottom: 1rem">
-    <RouterLink to="/" style="margin-right: 1rem">Home</RouterLink>
-    <RouterLink to="/ativos" style="margin-right: 1rem">Ativos</RouterLink>
-    <RouterLink to="/corretoras" style="margin-right: 1rem">Corretoras</RouterLink>
-    <RouterLink to="/tiposOrdens" style="margin-right: 1rem">Tipos de Ordens</RouterLink>
-    <RouterLink to="/transacoes" style="margin-right: 1rem">Transações</RouterLink>
+  <nav>
+    <RouterLink to="/" class="nav__item">Home</RouterLink>
+    <RouterLink to="/ativos" class="nav__item">Ativos</RouterLink>
+    <RouterLink to="/corretoras" class="nav__item">Corretoras</RouterLink>
+    <RouterLink to="/tiposOrdens" class="nav__item">Tipos de Ordens</RouterLink>
+    <RouterLink to="/transacoes" class="nav__item">Transações</RouterLink>
   </nav>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router'
 </script>
+
+<style scoped>
+nav {
+  padding: 1rem;
+  background-color: black;
+  margin-bottom: 1rem;
+}
+.nav__item {
+  margin-right: 1rem;
+  text-decoration: none;
+  color: white;
+}
+.router-link-active {
+  color: red;
+}
+</style>
