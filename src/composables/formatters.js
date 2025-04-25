918390d38c917981517ac8974f3921a4aa850e84
hemilioaraujo <hemilioaraujo@gmail.com>
2025-04-24 22:33:59 -0300

    feat: add cnpj formatter

--- src/composables/formatters.js
+++ src/composables/formatters.js
@@ -10,7 +10,11 @@ export function useFormatters() {
     date: (data) => {
       if (!data) return '-'
 
-      return new Date(data + 'T00:00:00').toLocaleDateString('pt-BR') 
+      return new Date(data + 'T00:00:00').toLocaleDateString('pt-BR')
+    },
+
+    cnpj: (cnpj) => {
+      return cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5')
     },
   }
 }
